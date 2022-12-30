c9d4e511fed448f91c9011e0d1b090ba81e5c19b
Fang Xuetao <fxtaoo@gmail.com>
2022-12-30 14:45:55 +0800

    重命名函数

--- aliyun/oss.go
+++ aliyun/oss.go
@@ -1,14 +1,31 @@
 // 阿里云相关
 package aliyun
 
+import "github.com/aliyun/aliyun-oss-go-sdk/oss"
+
+type OSSConf struct {
+	Endpoint        string `json:"endpoint"`
+	AccessKeyId     string `json:"accessKeyId"`
+	AccessKeySecret string `json:"accessKeySecret"`
+}
+
 type OSSFile struct {
 	BucketName     string `json:"bucketName"`
 	BucketFilePath string `json:"bucketFilePath"`
 	LoadFilePath   string `json:"loadFilePath"`
 }
 
+// 返回 OSSClient
+func (c *OSSConf) OSSClient() (*oss.Client, error) {
+	client, err := oss.New(c.Endpoint, c.AccessKeyId, c.AccessKeySecret)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
 // 上传文件（文件存在覆盖）
-func (f *OSSFile) UploadFile(conf *Conf) error {
+func (f *OSSFile) UploadFile(conf *OSSConf) error {
 	client, err := conf.OSSClient()
 	if err != nil {
 		return err
